232e5d8670548bf2220ca7d8b2fb41020518a202
Shivani Gattigorla <84455443+GattigorlaShivani@users.noreply.github.com>
2022-12-19 17:05:54 +0530

    Update content.py

--- Capstone_Project1/service/content.py
+++ Capstone_Project1/service/content.py
@@ -8,11 +8,9 @@ TOKEN = os.getenv("TOKEN")
 
 
 def get_data():
-    """Sheety API Manage"""
     headers = {
         "Authorization": f"Bearer {TOKEN}"
     }
-
     res = requests.get(url=URL, headers=headers)
     res.raise_for_status()
     return res.json()
